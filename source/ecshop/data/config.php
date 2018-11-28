47963bdc806dff230e20164343ae47b3bd5af3d5
yunyichh <yunyichh@outlook.com>
2018-11-28 15:14:17 +0800

    20181128

---
+++ source/ecshop/data/config.php
@@ -0,0 +1,37 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "ecshop3.6";
+
+// database username
+$db_user   = "root";
+
+// database password
+$db_pass   = "root";
+
+// table prefix
+$prefix    = "ecs_";
+
+$timezone    = "PRC";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '');
+
+define('STORE_KEY','412f813dea29f20fa2850ab9632820b7');
+
+?>
